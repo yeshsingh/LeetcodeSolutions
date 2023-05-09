362b9cdf9eaaf8c96c50cf05365ad618b9376a55
Yesh Singh <121896611+yeshsingh@users.noreply.github.com>
2023-05-10 02:09:49 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (30.23%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
         int m = matrix.size(), n = matrix[0].size();
-    vector<int> ans;
- 
-    if (m == 0)
+        vector<vector<bool> > seen(m, vector<bool>(n, false));
+        int d1[]={0 ,1 ,0, -1};
+        int d2[]={1 ,0 ,-1, 0};
+        int x=0,y=0,di=0;
+        for(int i=0;i<m*n;i++)
+        {
+            ans.push_back(matrix[x][y]);
+            seen[x][y]=true;
+            int newx=x+d1[di];
+            int newy=y+d2[di];
+            if(0 <= newx && newx < m && 0 <= newy && newy < n
+            && seen[newx][newy]==false)
+            {
+                x=newx;
+                y=newy;
+            }
+            else 
+            {
+                di=(di+1)%4;
+                x+=d1[di];
+                y+=d2[di];
+            }
+        }
         return ans;
- 
-    vector<vector<bool> > seen(m, vector<bool>(n, false));
-    int dr[] = { 0, 1, 0, -1 };
-    int dc[] = { 1, 0, -1, 0 };
- 
-    int x = 0, y = 0, di = 0;
-    for (int i = 0; i < m * n; i++) {
-        ans.push_back(matrix[x][y]);
-        seen[x][y] = true;
-        int newX = x + dr[di];
-        int newY = y + dc[di];
- 
-        if (0 <= newX && newX < m && 0 <= newY && newY < n
-            && !seen[newX][newY]) {
-            x = newX;
-            y = newY;
-        }
-        else {
-            di = (di + 1) % 4;
-            x += dr[di];
-            y += dc[di];
-        }
-    }
-    return ans;
     }
 };
