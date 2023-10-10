de4e3cc54873d35e16a6cf5f52a75938fc694235
Yesh Singh <121896611+yeshsingh@users.noreply.github.com>
2023-10-10 21:08:36 +0530

    Time: 3 ms (42.65%), Space: 6.3 MB (22.22%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    double myPow(double x, int n) {
+        
+        if(n==0){
+            return 1;
+        }
+        if (n < 0) { 
+            n = abs(n);
+            x = 1/x;
+        }
+        if(n%2==0){
+            return myPow(x*x,n/2);
+        
+        }else{
+            return x*myPow(x*x,n/2);
+        }
+    }
+};/*class Solution {
+public:
     double ans(double x, int n)
     {
         if(n==0)
@@ -28,4 +46,4 @@ public:
         
         return d;
     }
-};
+};*/
