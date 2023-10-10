f8a2c24d44845bc1dfa3c9551266469df4404833
Yesh Singh <121896611+yeshsingh@users.noreply.github.com>
2023-10-10 21:07:05 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (13.31%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double ans(double x, int n)
+    {
+        if(n==0)
+        {
+            return 1.0000;
+        }
+        if(n<0)
+        {
+            return 1/x*ans(1/x,-(n+1));
+        }
+        if(n==2)
+        {
+            return x*x;
+        }
+        if(n%2==0)
+        {
+            return ans(ans(x,n/2),2);
+        }
+        else 
+        { 
+        return x*ans(ans(x,n/2),2);
+        }
+    }
+    double myPow(double x, int n) {
+        double d= ans(x,n);
+        
+        return d;
+    }
+};
