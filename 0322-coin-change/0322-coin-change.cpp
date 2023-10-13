cc8f7889679c3a3fc6c32fa7335ffb3f4705adf5
Yesh Singh <121896611+yeshsingh@users.noreply.github.com>
2023-10-13 16:02:43 +0530

    Time: 92 ms (33.27%), Space: 13.9 MB (87.00%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    int dp[12 + 1][10000 + 1];
+    int ans(int i,vector<int>& coins, int amount)
+    {
+        if(amount<=0 || coins.size()<=i)
+        {
+            if(amount==0)
+            {
+                return 0;
+            }
+            else 
+            {
+                return INT_MAX-1;
+            }
+        }
+        if(dp[i][amount]!=-1)
+        {
+            return dp[i][amount];
+        }
+        if(amount<coins[i])
+        { 
+        int take2=ans(i+1,coins,amount);
+        dp[i][amount]=(take2);
+        }
+        else 
+        {
+            int take1=1+ans(i,coins,amount-coins[i]);
+            int take2=ans(i+1,coins,amount);
+            dp[i][amount]=min({take2,take1});
+        }
+        return dp[i][amount];
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        memset(dp, -1, sizeof(dp));
+        int a1=ans(0,coins,amount);
+        if(a1==INT_MAX-1)
+        {
+            return -1;
+        }
+        else 
+        {
+            return a1;
+        }
+    }
+};
